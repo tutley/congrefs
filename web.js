bd94404ab8611b0291bdcc26f9277a68d4ab1750
Tom Utley <tom@tomutley.com>
2013-04-03 16:56:26 -0400

    mongodb

--- web.js
+++ web.js
@@ -1,6 +1,21 @@
 var express = require("express");
 var app = express();
 
+// DB Stuff
+var mongo = require('mongodb');
+
+var mongoUri = process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/mydb'; 
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
+});
+
+
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
